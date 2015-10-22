7b419817674d99e205d9598fae64d83ccca27768
kiler129 <grzegorz@noflash.pl>
2015-10-22 12:25:28 +0200

    Added configuration for RSS aggregator.

---
+++ Configuration/RssAggregatorConfiguration.php
@@ -0,0 +1,178 @@
+<?php
+
+namespace noFlash\TorrentGhost\Configuration;
+
+
+/**
+ * Class holding configuration for
+ */
+class RssAggregatorConfiguration extends AggregatorAbstractConfiguration
+{
+    /**
+     * @var string|null URL of RSS feed
+     */
+    protected $url;
+
+    /**
+     * @var int Specified, in seconds, how often feed should be fetched. By default 300s.
+     */
+    protected $interval = 300;
+
+    /**
+     * @var string Name of tag where torrent name is located, by default it's "title".
+     */
+    protected $nameTagName = 'title';
+
+    /**
+     * @var string Name of tag where torrent link is located, by default it's "link".
+     */
+    protected $linkTagName = 'link';
+
+    /**
+     * Provides URL of RSS feed to fetch.
+     *
+     * @return string|null
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Sets URL of RSS feed to fetch.
+     *
+     * @param string $url Full URL with one of the following schemas: http, https, ftp, ftps.
+     *
+     * @throws \InvalidArgumentException Thrown in case of invalid URL.
+     */
+    public function setUrl($url)
+    {
+        if (!preg_match("
+              /^                                                      # Start at the beginning of the text
+              (?:https?|ftps?):\/\/                                   # Look for http, https, ftp or ftps schema
+              (?:                                                     # Userinfo (optional) which is typically
+                (?:(?:[\w\.\-\+!$&'\(\)*\+,;=]|%[0-9a-f]{2})+:)*      # a username or a username and password
+                (?:[\w\.\-\+%!$&'\(\)*\+,;=]|%[0-9a-f]{2})+@          # combination
+              )?
+              (?:
+                (?:[a-z0-9\-\.]|%[0-9a-f]{2}|[\p{L}])+                # A domain name or a IPv4 address
+                |(?:\[(?:[0-9a-f]{0,4}:)*(?:[0-9a-f]{0,4})\])         # or a well formed IPv6 address
+              )
+              (?::[0-9]+)?                                                  # Server port number (optional)
+              (?:[\/|\?]
+                (?:[\w#!:\.\?\+=&@$'~*,;\/\(\)\[\]\-]|%[0-9a-f]{2}|\p{L})   # The path and query (optional)
+              *)?
+            $/xiu", $url)
+        ) {
+            throw new \InvalidArgumentException('Invalid RSS feed URL');
+        }
+
+        $this->url = $url;
+    }
+
+    /**
+     * Provides RSS fetch interval.
+     *
+     * @return int Number of seconds.
+     */
+    public function getInterval()
+    {
+        return $this->interval;
+    }
+
+    /**
+     * Sets number of seconds between each RSS fetch.
+     *
+     * @param int $interval Number of seconds.
+     */
+    public function setInterval($interval)
+    {
+        if (!is_int($interval) || $interval < 1) {
+            throw new \InvalidArgumentException('Specified interval value cannot be used - it should be integer not smaller than 1');
+        }
+
+        $this->interval = $interval;
+    }
+
+    /**
+     * Provides tag name where name is located.
+     *
+     * @return string
+     */
+    public function getNameTagName()
+    {
+        return $this->nameTagName;
+    }
+
+    /**
+     * Sets tag name where to look for name.
+     *
+     * @param string $nameTagName Any valid XML tag.
+     *
+     * @throws \InvalidArgumentException Thrown if invalid tag name was specified.
+     */
+    public function setNameTagName($nameTagName)
+    {
+        if (!preg_match($this->getTagNameRegex(), $nameTagName)) {
+            throw new \InvalidArgumentException('Invalid tagName for name');
+        }
+
+        $this->nameTagName = $nameTagName;
+    }
+
+    /**
+     * Provides regex for use with preg_match() to check given XML tag name.
+     *
+     * @return string
+     */
+    private function getTagNameRegex()
+    {
+        return '~
+                    # XML 1.0 Name symbol PHP PCRE regex <http://www.w3.org/TR/REC-xml/#NT-Name>
+                    (?(DEFINE)
+                        (?<NameStartChar> [:A-Z_a-z\\xC0-\\xD6\\xD8-\\xF6\\xF8-\\x{2FF}\\x{370}-\\x{37D}\\x{37F}-\\x{1FFF}\\x{200C}-\\x{200D}\\x{2070}-\\x{218F}\\x{2C00}-\\x{2FEF}\\x{3001}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFFD}\\x{10000}-\\x{EFFFF}])
+                        (?<NameChar>      (?&NameStartChar) | [.\\-0-9\\xB7\\x{0300}-\\x{036F}\\x{203F}-\\x{2040}])
+                        (?<Name>          (?&NameStartChar) (?&NameChar)*)
+                    )
+                    ^(?&Name)$
+                ~ux';
+    }
+
+    /**
+     * Provides tag name where link is located.
+     *
+     * @return string
+     */
+    public function getLinkTagName()
+    {
+        return $this->linkTagName;
+    }
+
+    /**
+     * Sets tag name where to look for link.
+     *
+     * @param string $nameTagName Any valid XML tag.
+     *
+     * @throws \InvalidArgumentException Thrown if invalid tag name was specified.
+     */
+    public function setLinkTagName($linkTagName)
+    {
+        if (!preg_match($this->getTagNameRegex(), $linkTagName)) {
+            throw new \InvalidArgumentException('Invalid tagName for link');
+        }
+
+        $this->linkTagName = $linkTagName;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isValid()
+    {
+        if ($this->url === null) {
+            return false;
+        }
+
+        return parent::isValid();
+    }
+}
---
+++ Tests/Configuration/RssAggregatorConfigurationTest.php
@@ -0,0 +1,203 @@
+<?php
+
+namespace noFlash\TorrentGhost\Tests\Configuration;
+
+
+use noFlash\TorrentGhost\Configuration\RssAggregatorConfiguration;
+
+class RssAggregatorConfigurationTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var RssAggregatorConfiguration
+     */
+    private $subjectUnderTest;
+
+    public function setUp()
+    {
+        $this->subjectUnderTest = new RssAggregatorConfiguration();
+    }
+
+    public function testClassExtendsAggregatorAbstractConfiguration()
+    {
+        $this->assertInstanceOf('\noFlash\TorrentGhost\Configuration\AggregatorAbstractConfiguration',
+            $this->subjectUnderTest);
+    }
+
+    public function testUrlIsNullByDefault()
+    {
+        $this->assertNull($this->subjectUnderTest->getUrl());
+    }
+
+    public function validUrlsProvider()
+    {
+        return [
+            ['http://example.org'],
+            ['https://example.org'],
+            ['http://example.org/'],
+            ['https://example.org/'],
+            ['http://example.org/test'],
+            ['http://example.org/test?x=1&y=2'],
+            ['http://test:test@example.org/'],
+            ['http://test:test@example.org:88/'],
+            ['http://test@example.org/'],
+            ['http://idn-name-with-ąćół-characters.pl/'],
+            ['http://xn--punnycode-name-with--characters-z9c70dec04j.pl/'],
+            ['http://example.org/żółty-kot'],
+            ['http://example.pizza'],
+            ['http://example.corporate'],
+            ['ftp://example.corporate'],
+            ['ftps://example.corporate'],
+            ['http://127.0.0.1/feed.rss'],
+            ['https://203.0.113.123/feed.rss'],
+            ['ftp://127.0.0.1/feed.rss'],
+            ['ftps://203.0.113.123/feed.rss'],
+        ];
+    }
+
+    /**
+     * @dataProvider validUrlsProvider
+     */
+    public function testUrlAcceptsValidValues($url)
+    {
+        $this->subjectUnderTest->setUrl($url);
+        $this->assertSame($url, $this->subjectUnderTest->getUrl());
+    }
+
+    public function invalidOrDangerousUrlsProvider()
+    {
+        return [
+            ['derp-derp-derp'],
+            ['/derp'],
+            ['derp.pl'],
+            ['http://ex am ple.org'],
+            ['https://ex am ple.org/'],
+            ['ftp://ex am ple.org'],
+            ['ftps://ex am ple.org/'],
+            ['file:///etc/hosts'],
+            ['javascript://aa'],
+            ['javascript://test%0Aalert(321)'],
+            [null]
+        ];
+    }
+
+    /**
+     * @dataProvider invalidOrDangerousUrlsProvider
+     */
+    public function testUrlSetterRejectsInvalidValues($url)
+    {
+        $this->setExpectedException('\InvalidArgumentException');
+        $this->subjectUnderTest->setUrl($url);
+    }
+
+    public function testIntervalIsSetToFiveMinutesByDefault()
+    {
+        $this->assertSame(300, $this->subjectUnderTest->getInterval());
+    }
+
+    public function testIntervalAcceptsValidTimes()
+    {
+        $this->subjectUnderTest->setInterval(10);
+        $this->assertSame(10, $this->subjectUnderTest->getInterval());
+
+        $this->subjectUnderTest->setInterval(3000);
+        $this->assertSame(3000, $this->subjectUnderTest->getInterval());
+
+        $this->subjectUnderTest->setInterval(1);
+        $this->assertSame(1, $this->subjectUnderTest->getInterval());
+
+        $this->subjectUnderTest->setInterval(PHP_INT_MAX);
+        $this->assertSame(PHP_INT_MAX, $this->subjectUnderTest->getInterval());
+    }
+
+    public function testIntervalThrowsInvalidArgumentExceptionWhenFloatWasPassed()
+    {
+        $this->setExpectedException('\InvalidArgumentException');
+        $this->subjectUnderTest->setInterval(1.0);
+    }
+
+    public function testIntervalThrowsInvalidArgumentExceptionWhenZeroWasPassed()
+    {
+        $this->setExpectedException('\InvalidArgumentException');
+        $this->subjectUnderTest->setInterval(0);
+    }
+
+    public function testIntervalThrowsInvalidArgumentExceptionWhenValueBelowZeroWasPassed()
+    {
+        $this->setExpectedException('\InvalidArgumentException');
+        $this->subjectUnderTest->setInterval(-5);
+    }
+
+    public function validXmlTagsProvider()
+    {
+        return [
+            ['title'],
+            ['test'],
+            ['sample-longer-tag'],
+        ];
+    }
+
+    /**
+     * @dataProvider validXmlTagsProvider
+     */
+    public function testNameTagNameSetterAcceptsValidXmlTagNames($tagName)
+    {
+        $this->subjectUnderTest->setNameTagName($tagName);
+        $this->assertSame($tagName, $this->subjectUnderTest->getNameTagName());
+    }
+
+    public function testNameTagNameSetterRejectsTagsStartingWithNumber()
+    {
+        $this->setExpectedException('\InvalidArgumentException', 'Invalid tagName for name');
+        $this->subjectUnderTest->setNameTagName('1a');
+    }
+
+    public function testNameTagNameSetterRejectsTagsStartingWithDot()
+    {
+        $this->setExpectedException('\InvalidArgumentException', 'Invalid tagName for name');
+        $this->subjectUnderTest->setNameTagName('.xx');
+    }
+
+    public function testNameTagNameSetterRejectsTagsContainingSpaces()
+    {
+        $this->setExpectedException('\InvalidArgumentException', 'Invalid tagName for name');
+        $this->subjectUnderTest->setNameTagName('test tag');
+    }
+
+    /**
+     * @dataProvider validXmlTagsProvider
+     */
+    public function testLinkTagNameSetterAcceptsValidXmlTagNames($tagName)
+    {
+        $this->subjectUnderTest->setLinkTagName($tagName);
+        $this->assertSame($tagName, $this->subjectUnderTest->getLinkTagName());
+    }
+
+    public function testLinkTagNameSetterRejectsTagsStartingWithNumber()
+    {
+        $this->setExpectedException('\InvalidArgumentException', 'Invalid tagName for link');
+        $this->subjectUnderTest->setLinkTagName('1b');
+    }
+
+    public function testLinkTagNameSetterRejectsTagsStartingWithDot()
+    {
+        $this->setExpectedException('\InvalidArgumentException', 'Invalid tagName for link');
+        $this->subjectUnderTest->setLinkTagName('.yyyy');
+    }
+
+    public function testLinkTagNameSetterRejectsTagsContainingSpaces()
+    {
+        $this->setExpectedException('\InvalidArgumentException', 'Invalid tagName for link');
+        $this->subjectUnderTest->setLinkTagName('example tag name');
+    }
+
+    public function testConfigurationIsConsideredInvalidByDefault()
+    {
+        $this->assertFalse($this->subjectUnderTest->isValid());
+    }
+
+    public function testConfigurationIsConsideredValidAfterSettingUrl()
+    {
+        $this->subjectUnderTest->setUrl('http://example.org/feed.rss');
+        $this->assertTrue($this->subjectUnderTest->isValid());
+    }
+}
