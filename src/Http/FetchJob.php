13118adba53f21a016abde2321fdbd25f9c7abda
kiler129 <grzegorz@noflash.pl>
2015-10-24 12:01:30 +0200

    Added header to all files.

--- src/Http/FetchJob.php
+++ src/Http/FetchJob.php
@@ -1,4 +1,14 @@
 <?php
+/*
+ * This file is part of TorrentGhost project.
+ * You are using it at your own risk and you are fully responsible
+ *  for everything that code will do.
+ *
+ * (c) Grzegorz Zdanowski <grzegorz@noflash.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
 
 namespace noFlash\TorrentGhost\Http;
 
